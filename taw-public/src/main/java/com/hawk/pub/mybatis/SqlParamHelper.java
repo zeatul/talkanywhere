05b7c2923b5483cfe9d7455d4fae50eaf0ac6af3
unknown <zeatul@qq.com>
2015-11-10 23:34:54 +0800

    h

--- taw-public/src/main/java/com/hawk/pub/mybatis/SqlParamHelper.java
+++ taw-public/src/main/java/com/hawk/pub/mybatis/SqlParamHelper.java
@@ -1,9 +1,16 @@
 package com.hawk.pub.mybatis;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class SqlParamHelper {
 	
-	public Map<String,Object> generatePageParams(String orderBy , int offset ,int limit){
-		
+	public static Map<String,Object> generatePageParams(String orderBy , int offset ,int limit){
+		Map<String,Object> params = new HashMap<String,Object>();
+		params.put("_orderBy", orderBy);
+		params.put("_offset", offset);
+		params.put("_limit", limit);
+		return params;
 	}
 
 }
--- taw-scene-service/src/main/java/com/taw/scene/service/FootPrintService.java
+++ taw-scene-service/src/main/java/com/taw/scene/service/FootPrintService.java
@@ -4,22 +4,27 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.hawk.pub.mybatis.SqlParamHelper;
 import com.hawk.utility.check.CheckTools;
 import com.taw.pub.scene.request.QueryFootPrintParam;
 import com.taw.scene.domain.FootPrintDomain;
+import com.taw.scene.mapper.FootPrintMapper;
 
 @Service
 public class FootPrintService {
 	
+	@Autowired
+	private FootPrintMapper footPrintMapper;
+	
+	
 	public List<FootPrintDomain> search(long userId, QueryFootPrintParam queryFootPrintParam) throws Exception{
 		CheckTools.check(queryFootPrintParam);
-		Map<String,Object> params = new HashMap<String,Object>();
+		Map<String,Object> params = SqlParamHelper.generatePageParams("last_enter_time desc", queryFootPrintParam.getOffset(),queryFootPrintParam.getLimit());
 		params.put("userId", userId);
-		params.put("_orderBy", "last_enter_time desc");
-		params.put("_offset", queryFootPrintParam.getOffset());
-		params.put("_limit", queryFootPrintParam.getLimit());
+		return footPrintMapper.loadFootPrintDynamic(params);
 	}
 
 }
