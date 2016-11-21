fa0557116030603e88e23bdc4aff40da64220d53
zeratul <zeatul@qq.com>
2016-11-21 11:07:08 +0800

    emoji

--- taw-database/sql/emoji.sql
+++ taw-database/sql/emoji.sql
@@ -4,16 +4,12 @@ ALTER DATABASE pm CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 use tm;
 ALTER TABLE t_tm_conversation CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE t_tm_message CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
-
 ALTER TABLE t_tm_conversation CHANGE message message VARCHAR(1000) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE t_tm_message CHANGE message message VARCHAR(1000) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 use pm;
 ALTER TABLE t_pm_picture CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE t_pm_picture_comment CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
 ALTER TABLE t_pm_picture CHANGE description description VARCHAR(1000) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
 ALTER TABLE t_pm_picture_comment CHANGE content content VARCHAR(1000) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
